d963d8889f9039a02d34cb4d5f87ef3ce95ddf62
Stuart <stuart.carlisle@googlemail.com>
2019-04-26 11:45:16 +0100

    Added server and setup dev and production builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,48 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path:path.resolve(__dirname,'public'),
-        filename:'bundle.js'
-    }
-    ,
-    module:{
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: './src/app.js',
+        output: {
+          path: path.join(__dirname, 'public'),
+          filename: 'bundle.js'
+        },
+        module:{
+            rules: [{
                 loader:'babel-loader',
-                options:{
-                    presets:['env','react'],
-                    plugins:"transform-class-properties"
-                }
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                  use:[
+                    {
+                      loader: 'css-loader',
+                      options: {
+                        sourceMap: true
+                      }
+                    },
+                    {
+                      loader: 'sass-loader',
+                      options: {
+                        sourceMap: true
+                      }
+                    }
+                  ]
+                })
+              }]
+            },
+            plugins: [
+              CSSExtract
+            ],
+            devtool: isProduction ? 'source-map' : 'inline-source-map',
+            devServer: {
+              contentBase: path.join(__dirname, 'public'),
+              historyApiFallback: true
             }
-        },  
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader', 
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devServer:{
-        contentBase:path.resolve(__dirname,'public')
-    },
-    devtool:'cheap-module-eval-source-map'
-}
+          };
+        };
